e4c52d2cff5305e7b8d15eaa34cfdf67b78404e7
CustomFont <jjddff12@gmail.com>
2023-02-14 14:35:13 -0500

    installed react-bootstrap and react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React, { createContext } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
-// import Login from './components/Login'
+import Login from './components/Login'
 // import Registration from './components/Regestration'
 // import UserData from './components/UserData'
 // import LeaderPortal from './components/LeaderPortal'
-import AlertRoster from './components/AlertRoster'
+// import AlertRoster from './components/AlertRoster'
 
 export const ListContext = createContext([])
 
@@ -13,11 +13,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path='/' element={<Login />} />
-        <Route path='/registration' element={<Registration />} />
+        <Route path='/' element={<Login />} />
+        {/* <Route path='/registration' element={<Registration />} />
         <Route path='/userdata' element={<UserData />} />
-        <Route path='/leaderportal' element={<LeaderPortal />} /> */}
-        <Route path='/alertroster' element={<AlertRoster />} /> 
+        <Route path='/leaderportal' element={<LeaderPortal />} />
+        <Route path='/alertroster' element={<AlertRoster />} />  */}
       </Routes>
     </div>
   );
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react"
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import { ListContext } from "../App"
+
+export default function AlertRoster() {
+    const alertList = useContext(ListContext)
+    return (
+        <div className="alert-container">
+            <h1>Alert Roster</h1>
+            <Container>
+                <Row>
+                    {alertList.map(alert => {
+                        return (
+                            <h4>{alert.last_name}</h4>
+                        )
+                    })}
+                </Row>
+            </Container>
+        </div>
+    )
+}
