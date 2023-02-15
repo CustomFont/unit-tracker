70829fd496007ba217199890fe07313d8db8d34e
cybitalex <alex@LaMimi>
2023-02-15 15:16:31 -0500

    completed form usestate

--- frontend/src/components/registration.js
+++ frontend/src/components/registration.js
@@ -46,6 +46,15 @@ export default function Registration() {
         arrOfCompanies.push(companies[c])
     }
 
+    // function handleChange(event) {
+    //     const value = event.target.value;
+    //     setUserData({
+    //         ...userData,
+    //         [event.target.name]: value
+    //     })
+    // }
+
+
 
    return (
         <>
@@ -60,13 +69,13 @@ export default function Registration() {
             <Form>
                 <Form.Group className="mb-3">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Last Name" />
+                    <Form.Control type="text" placeholder="Last Name" name="lastName" value={userData.last_name} onChange={(e) => setUserData({last_name: e.target.value})} />
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="First Name" />
+                    <Form.Control type="text" placeholder="First Name" name="firstName" value={userData.first_name} onChange={(e) => setUserData({first_name: e.target.value})} />
                     <Form.Label>SSN</Form.Label>
-                    <Form.Control type="text" placeholder="Social Security Number" />
+                    <Form.Control type="text" placeholder="Social Security Number" name="ssn" value={userData.SSN} onChange={(e) => setUserData({SSN: e.target.value})} />
                     <Form.Label>DOD ID</Form.Label>
-                    <Form.Control type="text" placeholder="DOD ID" />
+                    <Form.Control type="text" placeholder="DOD ID" name="DODID" value={userData.DODID} onChange={(e) => setUserData({DODID: e.target.value})} />
                     <Form.Label>Rank</Form.Label>
                         <Form.Select>
                             <option>PV1</option>
@@ -89,22 +98,23 @@ export default function Registration() {
                     <Form.Label>Platoon</Form.Label>
                     <Form.Control type="text" placeholder="Platoon" />
                     <Form.Label>PMOS</Form.Label>
-                    <Form.Control type="text" placeholder="Primary MOS" />
+                    <Form.Control type="text" placeholder="Primary MOS" name="mos" value={userData.mos} onChange={(e) => setUserData({mos: e.target.value})} />
                     <Form.Label>Date of Birth</Form.Label>
-                    <Form.Control type="date" placeholder="Date of Birth" />
+                    <Form.Control type="date" placeholder="Date of Birth" name="dob" value={userData.DOB} onChange={(e) => setUserData({DOB: e.target.value})} />
                     <Form.Label>Age</Form.Label>
                     <Form.Control type="text" placeholder="Age" />
                     <Form.Label>Weight</Form.Label>
-                    <Form.Control type="text" placeholder="Weight" />
+                    <Form.Control type="text" placeholder="Weight" name="weight" value={userData.weight} onChange={(e) => setUserData({weight: e.target.value})} />
                     <Form.Label>Height</Form.Label>
-                    <Form.Control type="text" placeholder="Height in Centimeters" />
+                    <Form.Control type="text" placeholder="Height in Centimeters" name="height" value={userData.height} onChange={(e) => setUserData({height: e.target.value})} />
                     <Form.Label>Blood Type</Form.Label>
-                    <Form.Control type="text" placeholder="Blood Type" />
+                    <Form.Control type="text" placeholder="Blood Type" name="bloodtype" value={userData.blood_type} onChange={(e) => setUserData({blood_type: e.target.value})} />
                     <Form.Label>Phone Number</Form.Label>
-                    <Form.Control type="text" placeholder="Phone number" />
+                    <Form.Control type="text" placeholder="Phone number" name="phonenumber" value={userData.phone_number} onChange={(e) => setUserData({phone_number: e.target.value})} />
                     <Form.Label>Address</Form.Label>
-                    <Form.Control type="text" placeholder="Address" />
+                    <Form.Control type="text" placeholder="Address" name="address" value={userData.address} onChange={(e) => setUserData({address: e.target.value})} />
                 </Form.Group>
+                {console.log(userData)}
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
