429e93933dcd8ea158df74b5cf9deb80d5814c5d
CustomFont <jjddff12@gmail.com>
2023-02-15 15:09:40 -0500

    fixed incrementation on company_data seed

--- server/seeds/01_company_data.js
+++ server/seeds/01_company_data.js
@@ -5,10 +5,10 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('company_data').del()
-  await knex('company_data').insert([
-    {id: 1, company_name: '5-73CAV', registration_key: 'ACB123'},
-    {id: 2, company_name: '1-325AIR', registration_key: '1325AR'},
-    {id: 3, company_name: '2-325AIR', registration_key: 'BGD345'},   
-    {id: 4, company_name: '1-505PIR', registration_key: 'EJD288'}
-  ]);
+  await knex('company_data').insert({company_name: '5-73CAV', registration_key: 'ACB123'});
+  await knex('company_data').insert({ company_name: '1-325AIR', registration_key: '1325AR' });
+  await knex('company_data').insert({ company_name: '2-325AIR', registration_key: 'BGD345' });
+  await knex('company_data').insert({ company_name: '1-505PIR', registration_key: 'EJD288' });
 };
+
+
