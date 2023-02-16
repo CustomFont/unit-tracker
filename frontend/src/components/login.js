4ea441862210fac21c0097fcd37f90bf44d2fe13
CustomFont <jjddff12@gmail.com>
2023-02-16 14:19:55 -0500

    removed login button, now alertroster/ leaderportal buttons redirect if login successful

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {LinkContainer} from 'react-router-bootstrap'
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
     const [userLogin, setUserLogin] = useState({"DODID": undefined, "SSN": undefined})
+    const navigate = useNavigate();
 
     const onFormSubmit = e => {
         e.preventDefault()
         let stringifiedJSON = JSON.stringify(userLogin);
-        console.log(stringifiedJSON)
         fetch('http://localhost:8080/login', {
             method: 'POST',
             headers: {
@@ -18,17 +19,20 @@ export default function Login() {
             body: stringifiedJSON,
             withCredentials: true,
             credentials: 'include'
-        }).then(res => console.log(res.session))
+        }).then(res => {
+            if(res.status === 200){
+                if (e.target.id === "alertRosterButton"){
+                    navigate('/alertroster')
+                } else if (e.target.id === "leadersportal") {
+                    navigate('/leadersportal')
+                }
+            }
+        }) 
     }
-    const getallusers = () => {
-        fetch('http://localhost:8080/users', { credentials: 'include' })
-            .then(res => res.json())
-            .then(data => console.log(data))
-    }
+
     return (
         <>
-            <button onClick={getallusers}>get all users</button>
-            <Form onSubmit={onFormSubmit}>
+            <Form>
                 <Form.Group className="mb-3">
                     <Form.Label>UNIT</Form.Label>
                         <Form.Select> 
@@ -51,30 +55,24 @@ export default function Login() {
                     <Form.Label>Social Security Number</Form.Label>
                     <Form.Control type="password" placeholder="SSN" onChange={(e) => setUserLogin(userLogin => ({ ...userLogin, "SSN": e.target.value }))} value={userLogin.SSN} />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Login
-                </Button>
-            </Form>
-            <br />
-            <LinkContainer to='/alertroster'>
-                <Button variant="secondary" type="button">
+                <Button id="alertRosterButton" variant="secondary" type="submit" onClick={onFormSubmit}>
                     Alert Roster
                 </Button>
-            </LinkContainer>
-            <br />
+                <br />
+                <br />
+                <Button id="leadersportal" variant="danger" type="submit" onClick={onFormSubmit}>
+                    Leaders' Portal
+                </Button>
+            </Form>
+
             <br />
             <LinkContainer to='/registration'>
-                <Button variant="success" type="button">
+                <Button id="registrationButton" variant="success" type="button">
                     Registration
                 </Button>
             </LinkContainer>
             <br />
             <br />
-            <LinkContainer to='/leadersportal'>
-                <Button variant="danger" type="button">
-                    Leaders' Portal
-                </Button>
-            </LinkContainer>
         </>
     )
 }
