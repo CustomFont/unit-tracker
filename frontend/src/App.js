88593a0422b4adb791167edf9a7c29aca4f6bfa4
Robert <robert.chandler724@gmail.com>
2023-02-21 16:46:03 -0500

    started return to LP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Routes,  Route }   from 'react-router-dom';
 import Registration from './components/registration';
 import Splashpage from './components/splashpage';
 import AlertRoster from './components/AlertRoster';
-// import LeadersPortal from './components/LeadersPortal';
+import LeadersPortal from './components/LeadersPortal';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path='/update' element={<></>} />
           <Route path='/registration' element={<Registration />} />
           <Route path='/alertroster' element={<AlertRoster />} />
-          {/* <Route path='/leadersportal' element={<LeadersPortal />} /> */}
+          <Route path='/leadersportal' element={<LeadersPortal />} />
         </Routes>
       </div>
     </>
