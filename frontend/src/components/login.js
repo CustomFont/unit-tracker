880483827bac94de4a6a4078df32c6eff47e5fbe
CustomFont <jjddff12@gmail.com>
2023-02-22 13:29:00 -0500

    login button links to update page instead of registration

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -44,7 +44,7 @@ export default function Login() {
             }).then(res => {
                 if(res.status === 200){
                     if (e.target.id === "login") {
-                        navigate('/registration')
+                        navigate(`/update/${userLogin.DODID}`)
                     } else if (e.target.id === "alertRosterButton"){
                         navigate('/alertroster')
                     } else if (e.target.id === "leadersportal") {
@@ -53,7 +53,7 @@ export default function Login() {
                 } 
                 else if (res.status === 250){
                     if (e.target.id === "login") {
-                        navigate('/registration')
+                        navigate(`/update/${userLogin.DODID}`)
                     } else if (e.target.id === "alertRosterButton") {
                         navigate('/alertroster')
                     } else if (e.target.id === "leadersportal") {
