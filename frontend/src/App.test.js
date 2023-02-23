2d0a0f975c1d2e1e7b385e00fc40df3b2ebb799f
CustomFont <jjddff12@gmail.com>
2023-02-23 13:52:30 -0500

    tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { findByText, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import UpdateInfo from './components/UpdateInfo';
 
 test('renders Unit Tracker header', () => {
   render(
@@ -31,4 +33,39 @@ test('renders all necessary buttons', () => {
   );
   const buttons = screen.getAllByRole('button');
   expect(buttons.length).toBe(4);
+});
+
+test('login text boxes accept input', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const DODIDtextBox = screen.getByPlaceholderText(/Enter DOD ID/i);
+  const SSNtextBox = screen.getByPlaceholderText(/SSN/i);
+  userEvent.type(DODIDtextBox, "1928477713")
+  userEvent.type(SSNtextBox, "200992888")
+  expect(SSNtextBox.value).toBe("200992888")
+  expect(DODIDtextBox.value).toBe("1928477713")
+});
+
+test('change to update component after user text input and login button clicked', async () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const DODIDtextBox = screen.getByPlaceholderText(/Enter DOD ID/i);
+  const SSNtextBox = screen.getByPlaceholderText(/SSN/i);
+  userEvent.type(DODIDtextBox, "1928477713")
+  userEvent.type(SSNtextBox, "200992888")
+  const loginButton = screen.getByText(/Login/i)
+  await userEvent.click(loginButton)
+  render(
+  <BrowserRouter>
+    <UpdateInfo/>
+  </BrowserRouter>
+  )
+  const updateHeader = await screen.findByText(/Update Information/i)
+  expect(updateHeader).toBeInTheDocument()
 });
