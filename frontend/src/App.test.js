ae3c6e0d4cb21c6c201a4a38e3a752ff5c3cc372
CustomFont <jjddff12@gmail.com>
2023-02-23 11:32:31 -0500

    app render tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,34 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Unit Tracker header', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const header = screen.getByText(/Unit Tracker/i);
+  expect(header).toBeInTheDocument();
+});
+
+test('renders DODID and SSN text boxes', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const DODIDtextBox = screen.getByPlaceholderText(/Enter DOD ID/i);
+  const SSNtextBox = screen.getByPlaceholderText(/SSN/i);
+  expect(DODIDtextBox && SSNtextBox).toBeInTheDocument();
+});
+
+test('renders all necessary buttons', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const buttons = screen.getAllByRole('button');
+  expect(buttons.length).toBe(4);
 });
