9d735ef4bef19f576ece17b85e668ab6b5ca5d78
CustomFont <jjddff12@gmail.com>
2023-02-24 14:41:05 -0500

    fixed soldier-update to check pathname, not href

--- server/server.js
+++ server/server.js
@@ -39,8 +39,6 @@ app.use(async (req, res, next) => {
     if (req.path === '/login' && req.method === 'POST') {
         req.session.authenticated = false;
         next();
-    } else if( req.path === '/soldier-record/:DODID'){
-        next();
     } else if (req.path === '/units') {
         //req.session.authenticated = true;
         next();
