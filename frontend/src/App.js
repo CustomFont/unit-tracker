423492f905afdf822fc5a5811bda95db00481a76
CustomFont <jjddff12@gmail.com>
2023-02-24 14:43:51 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,11 +22,7 @@ function App() {
           <Route path='/registration' element={<Registration />} />
           <Route path='/alertroster' element={<AlertRoster />} />
           <Route path='/leadersportal' element={<LeadersPortal />} />
-<<<<<<< HEAD
-          <Route path='/confirm' element={<Confirm />} />
-=======
           <Route path='/update/:DODID' element={<UpdateInfo />} />
->>>>>>> 9d735ef4bef19f576ece17b85e668ab6b5ca5d78
         </Routes>
       </div>
     </>
